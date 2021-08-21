7dff0798ae20b51802fb8db014d06a0c874ab8fe
Ryuta Kamizono <kamipo@gmail.com>
2021-08-21 15:32:23 +0900

    remove comment

--- ruby/app.rb
+++ ruby/app.rb
@@ -589,43 +589,6 @@ module Isucondition
 
     # ISUの性格毎の最新のコンディション情報
     get '/api/trend' do
-       #character_list = db.query('SELECT `character` FROM `isu` GROUP BY `character`')
-       #
-       #res = character_list.map do |character|
-       #  isu_list = db.xquery('SELECT `id`, `jia_isu_uuid`, `name`, `character`, `jia_user_id`, `created_at`, `updated_at` FROM `isu` WHERE `character` = ?', character.fetch(:character))
-       #  character_info_isu_conditions = []
-       #  character_warning_isu_conditions = []
-       #  character_critical_isu_conditions = []
-       #
-       #  isu_list.each do |isu|
-       #    conditions = db.xquery('SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ? ORDER BY timestamp DESC', isu.fetch(:jia_isu_uuid)).to_a
-       #    unless conditions.empty?
-       #      isu_last_condition = conditions.first
-       #      condition_level = calculate_condition_level(isu_last_condition.fetch(:condition))
-       #      trend_condition = { isu_id: isu.fetch(:id), timestamp: isu_last_condition.fetch(:timestamp).to_i }
-       #      case condition_level
-       #      when 'info'
-       #        character_info_isu_conditions.push(trend_condition)
-       #      when 'warning'
-       #        character_warning_isu_conditions.push(trend_condition)
-       #      when 'critical'
-       #        character_critical_isu_conditions.push(trend_condition)
-       #      end
-       #    end
-       #  end
-       #
-       #  character_info_isu_conditions.sort! { |a,b| b.fetch(:timestamp) <=> a.fetch(:timestamp) }
-       #  character_warning_isu_conditions.sort! { |a,b| b.fetch(:timestamp) <=> a.fetch(:timestamp) }
-       #  character_critical_isu_conditions.sort! { |a,b| b.fetch(:timestamp) <=> a.fetch(:timestamp) }
-       #
-       #  {
-       #    character: character.fetch(:character),
-       #    info: character_info_isu_conditions,
-       #    warning: character_warning_isu_conditions,
-       #    critical: character_critical_isu_conditions,
-       #  }
-       #end
-
       character = nil
       character_info_isu_conditions = []
       character_warning_isu_conditions = []
