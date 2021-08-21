78c97cac91e8b451eb8c7eee0bf4b6f2bbf2da25
Ryuta Kamizono <kamipo@gmail.com>
2021-08-21 12:40:27 +0900

    bulk insert

--- ruby/app.rb
+++ ruby/app.rb
@@ -647,19 +647,30 @@ module Isucondition
         count = db.xquery('SELECT COUNT(*) AS `cnt` FROM `isu` WHERE `jia_isu_uuid` = ?', jia_isu_uuid).first
         halt_error 404, 'not found: isu' if count.fetch(:cnt).zero?
 
+        values = []
         json_params.each do |cond|
           timestamp = Time.at(cond.fetch(:timestamp))
           halt_error 400, 'bad request body' unless valid_condition_format?(cond.fetch(:condition))
 
-          db.xquery(
-            'INSERT INTO `isu_condition` (`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`) VALUES (?, ?, ?, ?, ?)',
-            jia_isu_uuid,
-            timestamp,
-            cond.fetch(:is_sitting),
-            cond.fetch(:condition),
-            cond.fetch(:message),
-          )
+          values << "('#{jia_isu_uuid}','#{timestamp.strftime('%Y-%m-%d %H:%M:%S')}','#{cond[:is_sitting]}','#{cond[:condition]}','#{cond[:message]}')"
         end
+
+        sql = "INSERT INTO `isu_condition` (`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`) VALUES #{values.join(",")}"
+        db.query(sql)
+
+      #  json_params.each do |cond|
+      #    timestamp = Time.at(cond.fetch(:timestamp))
+      #    halt_error 400, 'bad request body' unless valid_condition_format?(cond.fetch(:condition))
+
+      #    db.xquery(
+      #      'INSERT INTO `isu_condition` (`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`) VALUES (?, ?, ?, ?, ?)',
+      #      jia_isu_uuid,
+      #      timestamp,
+      #      cond.fetch(:is_sitting),
+      #      cond.fetch(:condition),
+      #      cond.fetch(:message),
+      #    )
+      #  end
       #end
 
       status 202
