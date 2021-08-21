b65daba66c1f683455e43602042d1f11a804e23b
soudai <sone@have-fun.tech>
2021-08-21 13:46:06 +0900

    Initializeでcacheを消す

--- ruby/app.rb
+++ ruby/app.rb
@@ -182,6 +182,7 @@ module Isucondition
         jia_service_url,
       )
 
+      system('rm -fr cache/isu/*', out: :err, exception: true)
       isu_list = db.xquery('SELECT jia_isu_uuid FROM isu').to_a
       isu_list.each do |isu|
         File.open("cache/isu/#{isu.jia_isu_uuid.b}", "w")
